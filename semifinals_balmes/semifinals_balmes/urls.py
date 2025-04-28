055c5e6042c083695ca13b15648bc2da3e871ae2
balmes <adrianbalmes211@gmail.com>
2025-04-28 11:41:37 +0800

    first commit

--- semifinals_balmes/semifinals_balmes/urls.py
+++ semifinals_balmes/semifinals_balmes/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from loginpage.views import log
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',log),
 ]
